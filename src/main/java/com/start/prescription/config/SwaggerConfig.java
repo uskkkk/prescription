f9c77c95ffc486e319e077ed622cc4bd6c57a7eb
uskkkk <dntjd4562@gmail.com>
2023-05-10 22:08:50 +0900

    springfox -> springdoc swagger 변경

--- src/main/java/com/start/prescription/config/SwaggerConfig.java
+++ src/main/java/com/start/prescription/config/SwaggerConfig.java
@@ -1,59 +1,23 @@
 package com.start.prescription.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2            // 기능을 활성화하는 어노테이션입니다.
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.start.prescription.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Swagger 테스트")
-                .description("swagger config 테스트")
-                .version("1.0")
-                .build();
-    }
-
-    /**
-     * Swagger2 버전은 http://localhost:8080/swagger-ui.html
-     * spring-security와 연결할 때 이 부분을 작성하지 않으면 404에러가 뜬다.
-     *
-     * 3.x 버전 부터는 swagger-ui 경로가 다르다고 합니다.
-     * http://localhost:8080/swagger-ui/index.html 로 접근해보세요
-     */
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        // -- Static resources
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css");
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("데모 프로젝트 API Document")
+                .version("v0.0.1")
+                .description("데모 프로젝트의 API 명세서입니다.");
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
     }
 }
--- src/main/java/com/start/prescription/controller/SwaggerController.java
+++ src/main/java/com/start/prescription/controller/SwaggerController.java
@@ -2,30 +2,21 @@ package com.start.prescription.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/v1/api/")
+@Tag(name = "prescription" , description = "prescription API 명세서 예시")
 public class SwaggerController {
 
-    @Operation(summary = "swagger 테스트" , description = "swagger 테스트입니다.", tags = {"SwaggerController"})
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(schema = @Schema(implementation = SwaggerController.class))),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
-            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
-    })
-    @GetMapping("/v1/api/home/{id}")
+    @Operation(summary = "prescription swagger 테스트" , description = "swagger 테스트입니다.")
+    @GetMapping("home/{id}")
     public ResponseEntity<String> swaggerTest(@Parameter(description = "ID" ,required = false, example = "hello") @PathVariable("id") String id) {
         return new ResponseEntity("hello" + id, HttpStatus.OK);
     }
